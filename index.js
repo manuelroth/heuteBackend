c3758c4d4655f36239a9c8e0b86abe3db9030cef
Manuel Roth <manuelroth@hotmail.ch>
2015-06-17 19:07:08 +0200

    Added connection code for redis

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
 var express = require('express');
+var redis = require('redis');
+var url = require('url');
 var app = express();
+
+var redisURL = url.parse(process.env.REDIS_URL);
+var client = redis.createClient(redisURL.port, redisURL.hostname);
+client.auth(redisURL.auth.split(":")[1]);
+
+var bar = "";
+client.set('foo', 'bar');
+client.get('foo', function(err, reply) {
+  bar = reply.toString();
+  console.log(reply.toString());
+});
 
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(request, response) {
-  response.send('Hello World!');
+  response.send('Hello World!' + " " + bar);
 });
 
 app.listen(app.get('port'), function() {
