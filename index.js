c35276a0674ae3409ea9ce59d31a0648cc1faafe
Manuel Roth <manuelroth@hotmail.ch>
2015-06-18 13:39:16 +0200

    Added all crawler function

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(express.static(__dirname + '/public'));
 
 
 var job = new CronJob({
-  cronTime: '* * * * * *',
+  cronTime: '0 * * * * *',
   onTick: function() {
     // Runs every weekday at 00:01:00 AM.
     
@@ -39,7 +39,7 @@ app.get('/', function(request, response) {
        if (err) {
            console.error('Error: Couldnt read venues off redit');
        } else {
-         console.log('Successfully read: ' + JSON.stringify(value));
+         console.log('Successfully read: ' + moment().format('MMMM Do YYYY, h:mm:ss a'));
          response.json(value);;
        }
   });
@@ -80,9 +80,9 @@ function crawl() {
           var data = $(this).children().first().children().first().children().eq(1).children();
           event.title = data.eq(5).text();
           event.link = 'http://www.grabenhalle.ch' + data.eq(5).attr('href');
-          //event.description = data.eq(10).children().first().children().eq(1).children().first().children().first().;
-        });
-        console.log(JSON.stringify(event));*/
+          event.description = data.eq(10).children().first().children().eq(1).children().first().children().first().;
+        });*/
+
         venues.grabenhalle = event;
     }
   }]);
@@ -102,6 +102,114 @@ function crawl() {
           event.description = data.eq(2).text();
         });
         venues.kugl = event;
+    }
+  }]);
+  
+  c.queue([{
+    uri: 'http://tankstell.ch/',
+    jQuery: true,
+    
+    // The global callback won't be called 
+    callback : function (error, result, $) {
+        var event = { name: 'TANKSTELL', color: 'balanced', title: '', link: '', description: ''};
+        
+        /*$('.event-item-holder').filter(function(){
+          var data = $(this).children().first().children().first().children();
+          event.title = data.eq(1).children().first().text();
+          event.link = data.eq(1).children().first().attr('href');
+          event.description = data.eq(2).text();
+        });*/
+        venues.tankstell = event;
+    }
+  }]);
+  
+   c.queue([{
+    uri: 'http://oya-bar.ch/',
+    jQuery: true,
+    
+    // The global callback won't be called 
+    callback : function (error, result, $) {
+        var event = { name: 'ØYA', color: 'energized', title: '', link: '', description: ''};
+        
+        /*$('.event-item-holder').filter(function(){
+          var data = $(this).children().first().children().first().children();
+          event.title = data.eq(1).children().first().text();
+          event.link = data.eq(1).children().first().attr('href');
+          event.description = data.eq(2).text();
+        });*/
+        venues.oya = event;
+    }
+  }]);
+  
+  c.queue([{
+    uri: 'http://treppenhaus.ch/',
+    jQuery: true,
+    
+    // The global callback won't be called 
+    callback : function (error, result, $) {
+        var event = { name: 'TREPPENHAUS', color: 'assertive', title: '', link: '', description: ''};
+        
+        /*$('.event-item-holder').filter(function(){
+          var data = $(this).children().first().children().first().children();
+          event.title = data.eq(1).children().first().text();
+          event.link = data.eq(1).children().first().attr('href');
+          event.description = data.eq(2).text();
+        });*/
+        venues.treppenhaus = event;
+    }
+  }]);
+  
+  c.queue([{
+    uri: 'http://www.militaerkantine.ch/de/microsites/kultur/',
+    jQuery: true,
+    
+    // The global callback won't be called 
+    callback : function (error, result, $) {
+        var event = { name: 'MILITÄRKANTINE', color: 'stable', title: '', link: '', description: ''};
+        
+        /*$('.event-item-holder').filter(function(){
+          var data = $(this).children().first().children().first().children();
+          event.title = data.eq(1).children().first().text();
+          event.link = data.eq(1).children().first().attr('href');
+          event.description = data.eq(2).text();
+        });*/
+        venues.militaerkantine = event;
+    }
+  }]);
+  
+  c.queue([{
+    uri: 'http://talhof.sg/',
+    jQuery: true,
+    
+    // The global callback won't be called 
+    callback : function (error, result, $) {
+        var event = { name: 'TALHOF', color: 'balanced', title: '', link: '', description: ''};
+        
+        /*$('.event-item-holder').filter(function(){
+          var data = $(this).children().first().children().first().children();
+          event.title = data.eq(1).children().first().text();
+          event.link = data.eq(1).children().first().attr('href');
+          event.description = data.eq(2).text();
+        });*/
+        venues.talhof = event;
+    }
+  }]);
+  
+   c.queue([{
+    uri: 'http://www.flon-sg.ch/',
+    jQuery: true,
+    
+    // The global callback won't be called 
+    callback : function (error, result, $) {
+        var event = { name: 'FLON', color: 'royal', title: '', link: '', description: ''};
+        
+        /*$('.event-item-holder').filter(function(){
+          var data = $(this).children().first().children().first().children();
+          event.title = data.eq(1).children().first().text();
+          event.link = data.eq(1).children().first().attr('href');
+          event.description = data.eq(2).text();
+        });*/
+        venues.flon = event;
     }
   }]);
   
