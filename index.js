70de2232a0a55e3e7a322b7af4fbeb94861cf392
Manuel Roth <manuelroth@hotmail.ch>
2015-06-18 19:23:45 +0200

    Changed crawler module

--- index.js
+++ index.js
@@ -3,8 +3,9 @@ var redis = require('redis');
 var url = require('url');
 var moment = require('moment');
 var CronJob = require('cron').CronJob;
-var Crawler = require('crawler');
+var Krawler = require('krawler');
 var cors = require('cors');
+var cheerio = require('cheerio');
 var app = express();
 
 var redisURL = url.parse(process.env.REDIS_URL);
@@ -19,16 +20,7 @@ var job = new CronJob({
   cronTime: '0 * * * * *',
   onTick: function() {
     // Runs every weekday at 00:01:00 AM.
-    
-    var venues = crawl();
-    client.hmset('venues', venues , function(err) {
-        if (err) {
-           // Something went wrong
-           console.error('Error: Couldnt write venues to redis');
-        } else {
-           console.log('Successfully written: '+ moment().format('MMMM Do YYYY, h:mm:ss a'));
-        }
-    });
+   crawl();
   },
   start: true,
   timeZone: "Europe/Zurich"
@@ -38,185 +30,113 @@ job.start();
 app.get('/', function(request, response) {
   client.hgetall('venues', function(err, value) {
        if (err) {
-           response.status(404);
-           response.send('Error: Couldnt fetch venues off redis')
-           console.error('Error: Couldnt fetch venues off redis');
+         response.status(404);
+         response.send('Error: Couldnt fetch venues off redis');
+         console.error('Error: Couldnt fetch venues off redis');
        } else {
-         console.log('Successfully read: ' + moment().format('MMMM Do YYYY, h:mm:ss a'));
+         console.log('Read successfully: ' + moment().format('MMMM Do YYYY, h:mm:ss a'));
          console.log(value);
          response.json(value);
        }
   });
 });
 
+
 function crawl() {
-  var venues = { "data" : [] };
-  var c = new Crawler({
-    maxConnections : 10,
-    // This will be called for each crawled page 
-    callback : function (error, result, $) {
-      
-    }
+   var content = [];
+   var urls = [
+    'http://www.palace.sg/',
+    'http://grabenhalle.ch/',
+    'http://kugl.ch/',
+    'http://tankstell.ch/',
+    'http://oya-bar.ch/',
+    'http://treppenhaus.ch/',
+    'http://www.militaerkantine.ch/de/microsites/kultur/',
+    'http://talhof.sg/',
+    'http://www.flon-sg.ch/'
+    ];
+     
+    var krawler = new Krawler;
+     
+    krawler
+        .queue(urls)
+        .on('data', function($, url, response) {
+          var body = response.body;
+          content.push({"url": url, "body": body});
+        })
+        .on('error', function(err, url) {
+          console.log("Error crawl: " + url);
+          content.push({"url": url, "body": ""});
+        })
+        .on('end', function() {
+            filterData(content);
+        });
+}
+
+function filterData(content) {
+    var venues = { "data" : [] };
+    console.log(content.length);
+    content.forEach(function(entry) {
+      switch(entry.url) {
+        case 'http://www.palace.sg/':
+          venues.data.push({ "name": "PALACE", "color": "stable", "title": "", "link": "", "description": ""});
+          break;
+        case 'http://grabenhalle.ch/':
+          venues.data.push({ "name": "GRABENHALLE", "color": "positive", "title": "", "link": "", "description": ""});
+          break;
+        case 'http://kugl.ch/':
+          venues.data.push(kuglFilter(entry.body));
+          break;
+        case 'http://tankstell.ch/':
+          venues.data.push({ "name": "TANKSTELL", "color": "balanced", "title": "", "link": "", "description": ""});
+          break;
+        case 'http://oya-bar.ch/':
+          venues.data.push({"name": "ØYA", "color": "energized", "title": "", "link": "", "description": ""});
+          break;
+        case 'http://treppenhaus.ch/':
+          venues.data.push({"name": "TREPPENHAUS", "color": "assertive", "title": "", "link": "", "description": ""});
+          break;
+        case 'http://www.militaerkantine.ch/de/microsites/kultur/':
+          venues.data.push({"name": "MILITÄRKANTINE", "stable": "stable", "title": "", "link": "", "description": ""});
+          break;
+        case 'http://talhof.sg/':
+          venues.data.push({"name": "TALHOF", "balanced": "balanced", "title": "", "link": "", "description": ""});
+          break;
+        case 'http://www.flon-sg.ch/':
+          venues.data.push({"name": "FLON", "color": "royal", "title": "", "link": "", "description": ""});
+          break;
+        default:
+          venues.data.push({"name": "", "color": "", "title": "", "link": "", "description": ""});
+      }
+    });
+    console.log(JSON.stringify(venues));
+    writeVenues(venues);
+}
+
+function kuglFilter(content) {
+  var $ = cheerio.load(content);
+  var event = { "name": "KUGL", "color": "calm", "title": "", "link": "", "description": ""};
+  $('.event-item-holder').filter(function(){
+    var data = $(this).children().first().children().first().children();
+    event.title = data.eq(1).children().first().text();
+    event.link = data.eq(1).children().first().attr('href');
+    event.description = data.eq(2).text();
   });
   
-  c.queue([{
-    uri: 'http://www.palace.sg/',
-    jQuery: true,
- 
-    // The global callback won't be called 
-    callback: function (error, result, $) {
-        var event = { "name": "PALACE", "color": "stable", "title": "", "link": "", "description": ""};
-        
-        venues.data.push(event);
-    }
-  }]);
-  
-  c.queue([{
-    uri: 'http://www.grabenhalle.ch/',
-    jQuery: true,
- 
-    // The global callback won't be called 
-    callback: function (error, result, $) {
-        var event = { "name": "GRABENHALLE", "color": "positive", "title": "", "link": "", "description": ""};
-        
-        /*$('div#content > table > tbody > tr > td > table:nth-child(1)').filter(function(){
-          var data = $(this).children().first().children().first().children().eq(1).children();
-          event.title = data.eq(5).text();
-          event.link = 'http://www.grabenhalle.ch' + data.eq(5).attr('href');
-          event.description = data.eq(10).children().first().children().eq(1).children().first().children().first().;
-        });*/
+  return event;
+}
 
-        venues.data.push(event);
-    }
-  }]);
-  
-  c.queue([{
-    uri: 'http://kugl.ch/',
-    jQuery: true,
-    
-    // The global callback won't be called 
-    callback : function (error, result, $) {
-        var event = { "name": "KUGL", "color": "calm", "title": "", "link": "", "description": ""};
-        
-        $('.event-item-holder').filter(function(){
-          var data = $(this).children().first().children().first().children();
-          event.title = data.eq(1).children().first().text();
-          event.link = data.eq(1).children().first().attr('href');
-          event.description = data.eq(2).text();
-        });
-        venues.data.push(event);
-    }
-  }]);
-  
-  c.queue([{
-    uri: 'http://tankstell.ch/',
-    jQuery: true,
-    
-    // The global callback won't be called 
-    callback : function (error, result, $) {
-        var event = { "name": "TANKSTELL", "color": "balanced", "title": "", "link": "", "description": ""};
-        
-        /*$('.event-item-holder').filter(function(){
-          var data = $(this).children().first().children().first().children();
-          event.title = data.eq(1).children().first().text();
-          event.link = data.eq(1).children().first().attr('href');
-          event.description = data.eq(2).text();
-        });*/
-        venues.data.push(event);
-    }
-  }]);
-  
-   c.queue([{
-    uri: 'http://oya-bar.ch/',
-    jQuery: true,
-    
-    // The global callback won't be called 
-    callback : function (error, result, $) {
-        var event =  {"name": "ØYA", "color": "energized", "title": "", "link": "", "description": ""};
-        
-        /*$('.event-item-holder').filter(function(){
-          var data = $(this).children().first().children().first().children();
-          event.title = data.eq(1).children().first().text();
-          event.link = data.eq(1).children().first().attr('href');
-          event.description = data.eq(2).text();
-        });*/
-        venues.data.push(event);
-    }
-  }]);
-  
-  c.queue([{
-    uri: 'http://treppenhaus.ch/',
-    jQuery: true,
-    
-    // The global callback won't be called 
-    callback : function (error, result, $) {
-        var event = {"name": "TREPPENHAUS", "color": "assertive", "title": "", "link": "", "description": ""};
-        
-        /*$('.event-item-holder').filter(function(){
-          var data = $(this).children().first().children().first().children();
-          event.title = data.eq(1).children().first().text();
-          event.link = data.eq(1).children().first().attr('href');
-          event.description = data.eq(2).text();
-        });*/
-        venues.data.push(event);
-    }
-  }]);
-  
-  c.queue([{
-    uri: 'http://www.militaerkantine.ch/de/microsites/kultur/',
-    jQuery: true,
-    
-    // The global callback won't be called 
-    callback : function (error, result, $) {
-        var event = {"name": "MILITÄRKANTINE", "stable": "stable", "title": "", "link": "", "description": ""};
-        
-        /*$('.event-item-holder').filter(function(){
-          var data = $(this).children().first().children().first().children();
-          event.title = data.eq(1).children().first().text();
-          event.link = data.eq(1).children().first().attr('href');
-          event.description = data.eq(2).text();
-        });*/
-        venues.data.push(event);
-    }
-  }]);
-  
-  c.queue([{
-    uri: 'http://talhof.sg/',
-    jQuery: true,
-    
-    // The global callback won't be called 
-    callback : function (error, result, $) {
-        var event = {"name": "TALHOF", "balanced": "balanced", "title": "", "link": "", "description": ""};
-        
-        /*$('.event-item-holder').filter(function(){
-          var data = $(this).children().first().children().first().children();
-          event.title = data.eq(1).children().first().text();
-          event.link = data.eq(1).children().first().attr('href');
-          event.description = data.eq(2).text();
-        });*/
-        venues.data.push(event);
-    }
-  }]);
-  
-   c.queue([{
-    uri: 'http://www.flon-sg.ch/',
-    jQuery: true,
-    
-    // The global callback won't be called 
-    callback : function (error, result, $) {
-        var event = {"name": "FLON", "royal": "royal", "title": "", "link": "", "description": ""};
-        
-        /*$('.event-item-holder').filter(function(){
-          var data = $(this).children().first().children().first().children();
-          event.title = data.eq(1).children().first().text();
-          event.link = data.eq(1).children().first().attr('href');
-          event.description = data.eq(2).text();
-        });*/
-        venues.data.push(event);
-    }
-  }]);
-  
-  return venues;
+
+function writeVenues(venues) {
+  client.del('venues');
+  client.hmset('venues', venues , function(err) {
+        if (err) {
+           // Something went wrong
+           console.error('Error: Couldnt write venues to redis');
+        } else {
+           console.log('Write successfully: '+ moment().format('MMMM Do YYYY, h:mm:ss a'));
+        }
+    });
 }
 
 app.listen(app.get('port'), function() {
